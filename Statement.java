777b068367568001720840b6ee97fe2f8e62881c
Kallyan Mombach <mombachkallyan@gmail.com>
2025-11-26 16:25:39 -0300

    Commit 17

--- Statement.java
+++ Statement.java
@@ -1,3 +1,19 @@
+import java.util.Enumeration;
+
 public abstract class Statement {
-    public abstract String value(Customer aCustomer);
+
+    public String value(Customer aCustomer) {
+        Enumeration<Rental> rentals = aCustomer.getRentals();
+        String result = header(aCustomer);
+        while (rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            result += eachLine(each);
+        }
+        result += footer(aCustomer);
+        return result;
+    }
+
+    protected abstract String header(Customer aCustomer);
+    protected abstract String eachLine(Rental aRental);
+    protected abstract String footer(Customer aCustomer);
 }
