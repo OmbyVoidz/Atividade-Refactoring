c92e1c9817147c95234b563459847c5746f1001e
Kallyan Mombach <mombachkallyan@gmail.com>
2025-11-26 15:36:43 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -27,8 +27,8 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            // determine amounts for each line (extraído)
-            thisAmount = amountFor(each);
+            // agora usa o método movido para Rental
+            thisAmount = each.getCharge();
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -48,30 +48,4 @@ public class Customer {
                   " frequent renter points";
         return result;
     }
-
-    // Método extraído (Refactoring 1)
-    private double amountFor(Rental aRental) {
-    double thisAmount = 0;
-
-    switch (aRental.getMovie().getPriceCode()) {
-        case Movie.REGULAR:
-            thisAmount += 2;
-            if (aRental.getDaysRented() > 2)
-                thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-            break;
-
-        case Movie.NEW_RELEASE:
-            thisAmount += aRental.getDaysRented() * 3;
-            break;
-
-        case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (aRental.getDaysRented() > 3)
-                thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-            break;
-    }
-
-    return thisAmount;
-}
-
 }
