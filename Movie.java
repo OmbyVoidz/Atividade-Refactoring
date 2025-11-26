242b7ff2d7480c8734541026e26b7d30c6d5655f
Kallyan Mombach <mombachkallyan@gmail.com>
2025-11-26 16:01:15 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -5,29 +5,40 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
-        _priceCode = priceCode;
-    }
-
-    public int getPriceCode() {
-        return _priceCode;
-    }
-
-    public void setPriceCode(int arg) {
-        _priceCode = arg;
+        setPriceCode(priceCode);
     }
 
     public String getTitle() {
         return _title;
     }
 
-    // Refactoring 7 — Passo 1: getCharge
+    public int getPriceCode() {
+        return _price.getPriceCode();
+    }
+
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
+    }
+
     public double getCharge(int daysRented) {
         double thisAmount = 0;
-        switch (_priceCode) {
+        switch (_price.getPriceCode()) {
             case REGULAR:
                 thisAmount += 2;
                 if (daysRented > 2)
@@ -47,9 +58,8 @@ public class Movie {
         return thisAmount;
     }
 
-    // Refactoring 7 — Passo 2: getFrequentRenterPoints
     public int getFrequentRenterPoints(int daysRented) {
-        if ((_priceCode == NEW_RELEASE) && daysRented > 1) {
+        if ((_price.getPriceCode() == NEW_RELEASE) && daysRented > 1) {
             return 2;
         }
         return 1;
